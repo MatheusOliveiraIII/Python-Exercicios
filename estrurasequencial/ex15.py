bb5f338f6138a779a1cc8d701c04f5cfa21fcb37
matheus <matheus@matheus-Vostro-3460.(none)>
2021-10-17 22:56:59 -0300

    resposta exercicio 15

---
+++ estrurasequencial/ex15.py
@@ -0,0 +1,20 @@
+qH = input("Quanto você ganha por hora: ")
+hT = input("Quantas horas você trabalhou: ")
+
+salarioB = qH * hT
+
+ir = (11/100.0 * salarioB)
+inss = (8/100.0 * salarioB)
+sindicato = (5/100.0 * salarioB)
+
+vT = ir + inss + sindicato
+salarioL = salarioB - vT
+
+print 'Seu salário bruto é',salarioB
+
+print 'Valor dos impostos:'
+print 'IR: ',ir
+print 'INSS: ',inss
+print 'Sindicato: ',sindicato
+
+print 'Seu salário liquido é: ',salarioL
