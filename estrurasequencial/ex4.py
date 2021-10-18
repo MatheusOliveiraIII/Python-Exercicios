4cf24a11ec4602b3be056c361302e6941befe9db
matheus <matheus@matheus-Vostro-3460.(none)>
2021-10-17 22:41:35 -0300

    resposta exercicio 4

---
+++ estrurasequencial/ex4.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite a nota do 1 bimestre :'))
+n2 = int(input('Digite a nota do 2 bimestre: '))
+n3 = int(input('Digite a nota do  3 bimestre: '))
+n4 = int(input('Digite a nota do 4 bimestre: '))
+m = n1 + n2 + n3 + n4
+d = m / 4
+print('O total das notas são {} a media foi  {}'.format(m, d))
