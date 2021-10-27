009aeb7c664c9007e8feefaf4e631e695a96f637
matheus <matheus@matheus-Vostro-3460.(none)>
2021-10-27 09:21:28 -0300

    resposta exercicio 16

--- estrurasequencial/ex16.py
+++ estrurasequencial/ex16.py
@@ -5,4 +5,4 @@ latas = (cobertura // 18)
 valor = latas ** 80 
 print(f'Para cobrir os {area}m que  deseja, você ira precisar de {cobertura} e irá custar rs {valor:.f}'.format(cobertura, latas, valor))
 
-??
+?
